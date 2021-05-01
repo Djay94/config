d6572977ce0cfbefe5428ec91a5ffb970b13c925
Djay94 <83493562+Djay94@users.noreply.github.com>
2021-05-01 18:07:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,14 +7,23 @@ sudo pacman -S lightdm lightdm-gtk-greeter
 sudo systemctl enable lightdm
 
 # install xorg and xmonad
-sudo pacman -S xorg xmonad xmonad-contrib xmobar
+sudo pacman -S xorg
+
+# install i3
+sudo pacman i3-gaps i3bloacks i3lock i3status
+
+# install polybar
+git clone https://aur.archlinux.org/polybar.git
+cd polybar
+makepkg -s -i -r -c
 
 # install other necessities
-sudo pacman -S dmenu picom nitrogen termite pcmanfm
+sudo pacman -S dmenu picom nitrogen xfce4-terminal pcmanfm
 
 # install other
-sudo pacman -S chromium archlinux-wallpaper
+sudo pacman -S firefox archlinux-wallpaper neofetch emacs
 
 # copy config files
-cp .xprofile ~/
-cp -r .xmonad ~/
+cp -r i3 ~/.config
+cp -r nitrogen ~/.config
+cp -r polybar ~/.config
