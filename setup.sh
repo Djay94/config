82344ada7a71057bc289a0715d411d60edf4bc7c
Djay94 <83493562+Djay94@users.noreply.github.com>
2021-05-01 10:39:28 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+# add user to audio group
+sudo gpasswd -a Djay audio
+sudo pacman -S alsa alsa-utils
 
+# install xmonad and other
+sudo pacman -S xorg xmonad xmonad-contrib xmobar dmenu picom nitrogen chromium terminator
+
+cp .xprofile ~/
+cp .xmonad ~/
