5926761f18fb0154278b11d37a59337bd84f8ddd
Djay94 <83493562+Djay94@users.noreply.github.com>
2021-05-15 12:10:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,46 +1,29 @@
-echo -
-echo add user to audio group
-echo -
 # add user to audio group
 sudo gpasswd -a djay audio
 sudo pacman -S alsa alsa-utils
-echo -
-echo lightdm install
-echo -
+
 # install and enable lightdm
 sudo pacman -S lightdm lightdm-gtk-greeter
 sudo systemctl enable lightdm
-echo -
-echo xorg install
-echo -
+
 # install xorg
 sudo pacman -S xorg
-echo -
-echo i3 install
-echo -
+
 # install i3
 sudo pacman i3-gaps i3blocks i3lock i3status
-echo -
-echo polybar install
-echo -
+
 # install polybar
 git clone https://aur.archlinux.org/polybar.git
 cd polybar
 makepkg -s -i -r -c
 cd ..
-echo -
-echo install other necessities
-echo -
-# install other necessities
-sudo pacman -S dmenu picom nitrogen xfce4-terminal pcmanfm
-echo -
-echo install other
-echo -
+
+# install i3 necessities
+sudo pacman -S dmenu picom nitrogen
+
 # install other
-sudo pacman -S firefox archlinux-wallpaper neofetch emacs vim
-echo -
-echo copy config
-echo -
+sudo pacman -S firefox archlinux-wallpaper neofetch emacs vim xfce4-terminal alacritty pcmanfm ark audacity blender make discord gimp htop mousepad obs-studio qbittorent vlc
+
 # copy config files
 cp -r i3 ~/.config
 cp -r nitrogen ~/.config
