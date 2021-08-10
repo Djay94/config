901fbca65a4db110673dc1cbe88dce6e7c09484a
Djay94 <83493562+Djay94@users.noreply.github.com>
2021-08-10 20:34:50 +0100

    Delete .xmonad directory

--- .xmonad/cornermode.sh
+++
@@ -1,6 +0,0 @@
-if grep -Fq "corner-radius = 10" /home/djay/.xmonad/picom.conf
-then
-	sed -i 's/corner-radius = 10/corner-radius = 0/' /home/djay/.xmonad/picom.conf
-else
-	sed -i 's/corner-radius = 0/corner-radius = 10/' /home/djay/.xmonad/picom.conf
-fi
