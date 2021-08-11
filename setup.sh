2e0892ad9dc668a29cb042e81bb1480194e978e7
Your Name <you@example.com>
2021-08-11 19:24:20 +0100

    Edited setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mv ./home/* ~/
 curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | sudo pacman-key -a -
 mv ./pacman.conf /etc/pacman.conf
 
-sudo pacman -Syyu amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers lyrebird make minecraft-launcher mpv nano nasm neofetch nitrogen obs-studio qbittorent qemu qemu-arch-extra ranger rofi sddm sfml spotify steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg yay youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa
+sudo pacman -Syyu amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers lyrebird make minecraft-launcher mpv nano nasm neofetch nitrogen obs-studio qbittorent qemu qemu-arch-extra ranger rofi sddm sfml spotify steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg yay youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
 
 mkdir temp
 cd temp
