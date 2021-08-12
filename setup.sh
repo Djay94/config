ad66db93f2ce8b26938ca168d2d762c10101e195
Your Name <you@example.com>
2021-08-12 17:13:21 +0100

    added spotify key and enabled sddm

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@
 
 curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | sudo pacman-key -a -
 sudo mv -fv ./pacman.conf /etc/pacman.conf
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import
 
-sudo pacman -Syyu amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers make mpv nano nasm neofetch nitrogen obs-studio qbittorrent qemu qemu-arch-extra ranger rofi sddm sfml steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
+sudo pacman -Syyu
+sudo pacman -S amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers make mpv nano nasm neofetch nitrogen obs-studio qbittorrent qemu qemu-arch-extra ranger rofi sddm sfml steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
 
 mkdir temp
 cd temp
@@ -14,7 +16,7 @@ cd pfetch
 makepkg -sirc
 cd ..
 git clone https://aur.archlinux.org/picom-jonaburg-git.git
-cd picom-johnburg-git
+cd picom-jonaburg-git
 makepkg -sirc
 cd ..
 git clone https://aur.archlinux.org/ttf-ms-fonts.git
@@ -40,4 +42,6 @@ cd ..
 
 
 cd ..
-rm -vr temp
+rm -vrf temp
+
+sudo systemctl enable sddm.service
