a98474ae271280007572b8b16d5c2962dd64b970
Your Name <you@example.com>
2021-08-12 16:13:02 +0100

    Corrected mistakes

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ mv ./.config/* ~/.config/
 mv ./home/* ~/
 
 curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | sudo pacman-key -a -
-mv ./pacman.conf /etc/pacman.conf
+mv -fv ./pacman.conf /etc/pacman.conf
 
-sudo pacman -Syyu amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers lyrebird make minecraft-launcher mpv nano nasm neofetch nitrogen obs-studio qbittorent qemu qemu-arch-extra ranger rofi sddm sfml spotify steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg yay youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
+sudo pacman -Syyu amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers lyrebird make minecraft-launcher mpv nano nasm neofetch nitrogen obs-studio qbittorrent qemu qemu-arch-extra ranger rofi sddm sfml spotify steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
 
 mkdir temp
 cd temp
@@ -25,5 +25,19 @@ git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg -sirc
 cd ..
+git clone https://aur.archlinux.org/lyrebird.git
+cd lyrebird
+makepkg -sirc
+cd ..
+git clone https://aur.archlinux.org/minecraft-launcher.git
+cd minecraft-launcher
+makepkg -sirc
+cd ..
+git clone https://aur.archlinux.org/spotify.git
+cd spotify
+makepkg -sirc
+cd ..
+
+
 cd ..
 rm -vr temp
