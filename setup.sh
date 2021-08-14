0382b437230e5a7b520da6668aab7ae3fbf307d6
Your Name <you@example.com>
2021-08-14 18:31:34 +0100

    -

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo mv -fv ./pacman.conf /etc/pacman.conf
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import
 
 sudo pacman -Syyu
-sudo pacman -S amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers make mpv nano nasm neofetch nitrogen obs-studio qbittorrent qemu qemu-arch-extra ranger rofi sddm sfml steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg youtube-dl zip zsh alsa alsa-utils pulseaudio pulseaudio-alsa mesa
+sudo pacman -S amd-ucode audacity blender cmake discord dunst ed edk2-ovmf feh firefox flameshot gedit gimp git glava htop kitty linux-headers make mpv nano nasm neofetch nitrogen obs-studio qbittorrent qemu qemu-arch-extra ranger rofi sfml steam thunar torbrowser-launcher ungoogled-chromium unzip vim virt-manager virtualbox wget wine xmonad xmonad-contrib xmonad-utils xorg youtube-dl zip zsh mesa base-devel
 
 mkdir temp
 cd temp
@@ -43,5 +43,3 @@ cd ..
 
 cd ..
 rm -vrf temp
-
-sudo systemctl enable sddm.service
